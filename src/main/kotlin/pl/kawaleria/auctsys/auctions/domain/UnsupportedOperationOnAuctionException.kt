551ec95a07d6226cc574acbede4ccc7a636e8b4c
nqriver <nqriver@gmail.com>
2023-07-30 21:59:47 +0200

    Delete unused class

--- src/main/kotlin/pl/kawaleria/auctsys/auctions/domain/UnsupportedOperationOnAuctionException.kt
+++
@@ -1,7 +0,0 @@
-package pl.kawaleria.auctsys.auctions.domain
-
-import org.springframework.http.HttpStatus
-import pl.kawaleria.auctsys.auctions.dto.responses.ApiException
-
-private const val MESSAGE_FORMAT = "Cannot perform %s on %s auction"
-

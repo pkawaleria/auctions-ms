7b87da9e8e66973d6866b35060b0f18e474a9c6f
Filip Kaminski <s95429@pollub.edu.pl>
2023-09-18 14:40:11 +0200

    Repaired async images verification

--- src/main/kotlin/pl/kawaleria/auctsys/verifications/ContentVerificationClient.kt
+++ src/main/kotlin/pl/kawaleria/auctsys/verifications/ContentVerificationClient.kt
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.core.io.Resource
 
 import org.springframework.http.MediaType.*
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
 
 @HttpExchange
+@EnableAsync
 interface ContentVerificationClient {
 
     @PostExchange("/image/verify_image", contentType = MULTIPART_FORM_DATA_VALUE, accept = [APPLICATION_JSON_VALUE])

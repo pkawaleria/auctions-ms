41a34fa24dcb8bedb74e3f321cb230bef26c99ae
nqriver <nqriver@gmail.com>
2023-10-18 21:18:59 +0200

    Fix changelog using mongo template

--- src/main/kotlin/pl/kawaleria/auctsys/configs/AuctionDatabaseChangeLog.kt
+++ src/main/kotlin/pl/kawaleria/auctsys/configs/AuctionDatabaseChangeLog.kt
@@ -2,14 +2,14 @@ package pl.kawaleria.auctsys.configs
 
 import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate
 import org.springframework.data.domain.Sort
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.index.Index
 
 @ChangeLog(order = "001")
 class AuctionDatabaseChangeLog {
     @ChangeSet(order = "001", id = "createCategoryIndicesOnAuction", author = "lukasz-karasek")
-    fun createAllCategoryIndices(mongoTemplate: MongoTemplate) {
+    fun createAllCategoryIndices(mongoTemplate: MongockTemplate) {
         // Index for categoryPath.pathElements.name
         val indexDefinition1 = Index("categoryPath.pathElements.name", Sort.Direction.ASC) // Using 1 for ASC
         mongoTemplate.indexOps("auctions").ensureIndex(indexDefinition1)
